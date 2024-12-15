55bb64684aa12845af48633401c34c991f410d95
Bastillan <kedzierski.jedrzej@gmail.com>
2024-12-15 22:05:09 +0100

    corrected ===

--- frontend/src/TasksPlayList.js
+++ frontend/src/TasksPlayList.js
@@ -12,8 +12,8 @@ const TasksPlayList = ({ tasks, handleBackToGamesList }) => {
 
     const handleNext = () => {
         if (currentIndex < tasks.length - 1) {
-            setIsNextDisabled(currentIndex == globalIndex - 1);
-            setIsSubmitDisabled(currentIndex != globalIndex - 1);
+            setIsNextDisabled(currentIndex === globalIndex - 1);
+            setIsSubmitDisabled(currentIndex !== globalIndex - 1);
             setCurrentIndex(currentIndex + 1);
         }
     };
