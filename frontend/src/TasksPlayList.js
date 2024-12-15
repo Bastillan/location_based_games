33030f9cba042e837cbcf3ec1778a6341e1b7976
Bastillan <kedzierski.jedrzej@gmail.com>
2024-12-15 19:23:50 +0100

    disabling buttons in tasks

--- frontend/src/TasksPlayList.js
+++ frontend/src/TasksPlayList.js
@@ -5,25 +5,32 @@ import './TasksPlayList.css';
 
 const TasksPlayList = ({ tasks, handleBackToGamesList }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [globalIndex, setGlobalIndex] = useState(0);
     const [isNextDisabled, setIsNextDisabled] = useState(true);
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
     const [answer, setAnswer] = useState("");
 
     const handleNext = () => {
         if (currentIndex < tasks.length - 1) {
+            setIsNextDisabled(currentIndex == globalIndex - 1);
+            setIsSubmitDisabled(currentIndex != globalIndex - 1);
             setCurrentIndex(currentIndex + 1);
-            setIsNextDisabled(true);
         }
     };
 
     const handlePrevious = () => {
         if (currentIndex > 0) {
             setCurrentIndex(currentIndex - 1);
+            setIsNextDisabled(false);
+            setIsSubmitDisabled(true);
         }
     };
 
     const handleSubmit = () => {
         if (tasks[currentIndex].correct_answer === answer) {
             setIsNextDisabled(currentIndex === tasks.length -1);
+            setGlobalIndex(globalIndex + 1);
+            setIsSubmitDisabled(true);
         }
     }
 
@@ -32,7 +39,7 @@ const TasksPlayList = ({ tasks, handleBackToGamesList }) => {
             <button className='mainBut powrot' onClick={handleBackToGamesList}>Wróć do listy gier</button>
             {tasks.length > 0 && tasks[currentIndex] && (
                 <div>
-                    <h3>Zadanie {currentIndex + 1}</h3>
+                    <h3>Zadanie {currentIndex + 1} current_index/global_index: {currentIndex}/{globalIndex}</h3>
                     <p>Opis zadania: {tasks[currentIndex].description}</p>
                     {tasks[currentIndex].image && (
                         <img src={tasks[currentIndex].image} alt="obraz" />
@@ -49,7 +56,7 @@ const TasksPlayList = ({ tasks, handleBackToGamesList }) => {
             </div>
             <div className="butons">
                 <button className="previous" onClick={() => handlePrevious()} disabled={currentIndex === 0}>Poprzednie</button>
-                <button className="submit" onClick={() => handleSubmit()}>Zatwierdź odpowiedź</button>
+                <button className="submit" onClick={() => handleSubmit()} disabled={isSubmitDisabled}>Zatwierdź odpowiedź</button>
                 <button className="next" onClick={() => handleNext()} disabled={isNextDisabled}>Następne</button>
             </div>
         </div>
