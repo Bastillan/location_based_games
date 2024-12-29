b6659f13785029a4a96e7a18f5dcd015a535f8be
Michal Pienkos <michal.pienkos.mp3@gmail.com>
2024-12-29 02:14:24 +0100

    fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.API_URL,
-        changeOrigin: true,
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/auth': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       },
-      '/auth': {
-        target: import.meta.env.API_URL,
-        changeOrigin: true,
-      }
-    },
   }
-})
+  });
+}
