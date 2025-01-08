385a810e598f2e3d75cd531a11853c69c34342eb
jkedzier <jedrzej.kedzierski.stud@pw.edu.pl>
2025-01-08 05:01:53 +0100

    Get appropriate user data

--- frontend/src/services/AuthProvider.jsx
+++ frontend/src/services/AuthProvider.jsx
@@ -1,16 +1,23 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import api from './api';
+import {jwtDecode} from 'jwt-decode';
 
 export const AuthContext = createContext(undefined);
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-    const getUserData = async () => {
+    const getUserData = async (access) => {
         try {
-            const response = await api.get('/api/users');
-            setUser(response.data[0]);
+            const decoded = jwtDecode(access);
+            console.log(decoded);
+            const userId = decoded.user_id;
+            console.log(userId);
+            const response = await api.get('/api/users/');
             console.log(response.data);
+            const response_user = response.data.filter((elem) => elem.user==userId);
+            console.log(response_user);
+            setUser(response_user[0]);
         } catch (error) {
             console.error("Failed to fetch user data:", error);
             setUser(null);
@@ -20,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     const login = (accessToken, refreshToken) => {
         localStorage.setItem('access', accessToken);
         localStorage.setItem('refresh', refreshToken);
-        getUserData();
+        getUserData(accessToken);
     }
 
     const logout = () => {
